c22ff2391e42bb97d02aa489010134de5fd9ce56
oluobamzy <oluobamzy@gmail.com>
2023-11-28 05:25:56 -0500

    add welcome screen

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import { borderRadius } from '@mui/system';
 
 export default function Welcome() {
   return (
@@ -29,12 +31,22 @@ export default function Welcome() {
             position: 'absolute',
             top: 0,
             left: 0,
-            zIndex: -1,
+            zIndex: -1, opacity:0.2
           }}
         >
           <source src="https://assets.mixkit.co/videos/preview/mixkit-yellow-flower-macro-close-up-1899-large.mp4" type="video/mp4" />
           {/* Add additional source elements for other video formats if needed */}
         </video>
+        <Box sx={{ position: 'relative', zIndex: 1, color: 'common.white', height: '80%', display:"flex", flexDirection:"column"}}>
+           <img
+             src="https://images.unsplash.com/photo-1509293010317-d1ef3416ed66?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+             alt="logo"
+             style={{ height: '100%', width: 'auto', borderRadius:"10%", alignSelf:"center" }}
+           />
+           <Button style={{color:"white", backgroundColor:"blue", width: "50%"}}>
+              Call a Friend
+            </Button> 
+         </Box>
         
         {/* Your content goes here */}
       </Container>
