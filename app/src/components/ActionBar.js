c06a03c861038637092e62ab9cffdb19b138bde2
oluobamzy <oluobamzy@gmail.com>
2023-12-06 03:30:47 -0500

    fix app bar

---
+++ app/src/components/ActionBar.js
@@ -0,0 +1,18 @@
+import "./ActionBar.css";
+const ActionBar = () => {
+  return (
+    <div className="action-bar">
+      <div className="action-bar__item">
+        <div className="text-area-action">
+          <h1>Social tool for personal gain</h1>
+          <h4>Buy Now</h4>
+        </div>
+        <div className="action-bar__item__img">
+        <img src="https://images.pexels.com/photos/5083568/pexels-photo-5083568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="home" />
+        </div>
+      </div>
+
+    </div>
+  );
+};
+export default ActionBar;
--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -1,54 +1,11 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-
-
-export default function Welcome({setIsWelcome}) {
-  const handleClick = () => {
-    setIsWelcome(false);
-  };
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container
-        sx={{
-          height: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          overflow: 'hidden',
-          position: 'relative',
-        }}
-        maxWidth="xl"
-      >
-        <video
-          autoPlay
-          loop
-          muted
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            zIndex: -1, opacity:0.2
-          }}
-        >
-          <source src="https://assets.mixkit.co/videos/preview/mixkit-yellow-flower-macro-close-up-1899-large.mp4" type="video/mp4" />
-          {/* Add additional source elements for other video formats if needed */}
-        </video>
-        <Box sx={{ position: 'relative', zIndex: 1, color: 'common.white', height: '60%', display:"flex", flexDirection:"column"}}>
-          <img src="https://media.istockphoto.com/id/1768244401/photo/mother-taking-her-daughter-to-school.jpg?s=612x612&w=0&k=20&c=ljJYP0cFUe6pbxIpSMe9HCMqcyEHeL990SfWWgnIspg=" alt="Logo" border="0" style={{width:"100%", height:"auto"}}/>
-           <Button style={{color:"white", backgroundColor:"blue", width: "100%"}} onClick={handleClick}>
-              Start Chatting
-            </Button> 
-         </Box>
-        
-        {/* Your content goes here */}
-      </Container>
-    </React.Fragment>
-  );
-}
+import ActionBar from './ActionBar';
+import Nav from './Nav';
+const Welcome = () => {
+      return (
+        <div>
+          <Nav />
+          <ActionBar />
+        </div>
+      );
+};
+export default Welcome;
