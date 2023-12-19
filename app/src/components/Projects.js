6d8c4d23d035c6894762218c8d6666361e4efd30
oluobamzy <oluobamzy@gmail.com>
2023-12-19 12:34:15 -0500

    adding imgs for readme

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -1,13 +1,18 @@
 import Footer from "./Footer";
+import Nav from "./Nav";
 import ProjectCard from "./ProjectCard";
 import "./projects.css";
 
 const Projects = () => {
     return (
         <div className="projects-container">
+            <Nav />
+            <h1>Projects</h1>
+            <div className="projects">
              <ProjectCard />
             <ProjectCard />
-            <ProjectCard />
+            </div>
+            
         </div>
     );
 };
